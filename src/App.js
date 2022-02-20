72d1ac3920caf57fe2295aeb334f0e8785a5ac67
Albert <albertgcsula@gmail.com>
2022-02-19 16:59:29 -0800

    update gitignore.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>ReactJS App on AWS Amplify.</p>
+        <p>ReactJS Starter App on AWS Amplify.</p>
       </header>
     </div>
   );
